24f6cd1e025aeed4accdacb5c6b3479214864e33
Brett Shwom <brett.shwom@nyu.edu>
2014-02-27 14:59:41 -0500

    promises and what not

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
+var Promise = require('micropromise'),
+    keyframeUidCounter = 0
+
 //the following would be really cool to put in a decorator...
-Array.prototype.slice.call(document.querySelectorAll('.mediaItem')).forEach(function(element) {
-  
-  //swap out for promise library later -- was having a hard time getting micropromise to work here
-  
-  var promise = (function () {
-    var callback
-    
-    return {
-      fulfill : function (val) {
-        if (callback) callback(val)
-      },
-      then : function(_callback) {
-        callback = _callback
-      }
-    }
-  })()
-  
+var Helper = function(element) {
+
   var transforms = {
     scale3d : '',
     translate3d : ''
   }
-  
+
   var transitions = {
     all : '',
     scale3d : '',
     translate3d : ''
   }
-  
+
   element.transitions = {}
   element.transforms = {}
-  
-  
+
+
   element.animate = function (options) {
-    
-      
+
+    var promise = new Promise
+
     var keyframesString =
       ['from', 'to'] //could be replaced with 0%, 50% etc.
         .map(function (keyframe) {
@@ -43,41 +32,41 @@ Array.prototype.slice.call(document.querySelectorAll('.mediaItem')).forEach(func
           return stringifiedKeyframes
         })
         .join(' ')
-    
+
     var keyframeUid = ++keyframeUidCounter
     var animationName = 'animate-d574bfb4' + keyframeUid; //name has part of a random guid to help ensure no name collisions
     var selector = "@-webkit-keyframes " + animationName;
     var rule = "{" + keyframesString + "}";
-    
+
     console.warn(keyframesString)
 
-    
+
     var animationEndHandler = function () {
-     
+
       element.removeEventListener('webkitAnimationEnd', animationEndHandler)
-      
+
       console.warn(options.persistedTransforms)
-      
+
       if (options.persistedTransforms) { //kinda gross because there might be a run of the event loop where the transform isnt applied and may result in a flash (though I havent observed this yet)
         Object.keys(options.persistedTransforms)
           .forEach(function(transform) {
             element.transforms[transform] = options.persistedTransforms[transform]
           })
       }
-      
+
       promise.fulfill()
-      
+
     }
-    
+
     element.addEventListener('webkitAnimationEnd', animationEndHandler)
-        
+
     window.document.styleSheets[0].insertRule(selector + rule, 0);
-    element.style.webkitAnimation = animationName + ' ' + options.duration + ' ' + options.easing + ' ' 
-    
+    element.style.webkitAnimation = animationName + ' ' + options.duration + ' ' + options.easing + ' '
+
     return promise;
-    
+
   }
-    
+
   function getTransformString() {
     return Object.keys(transforms)
       .map(function (transform) {
@@ -88,7 +77,7 @@ Array.prototype.slice.call(document.querySelectorAll('.mediaItem')).forEach(func
       })
       .join(' ')
   }
-  
+
   function getTransitionString() {
     return Object.keys(transitions)
       .map(function (transition) {
@@ -99,7 +88,7 @@ Array.prototype.slice.call(document.querySelectorAll('.mediaItem')).forEach(func
       })
       .join(' ')
   }
-  
+
   Object.defineProperty(element.transforms, 'scale3d',{
     get : function () {
       return transforms.scale3d;
@@ -118,7 +107,7 @@ Array.prototype.slice.call(document.querySelectorAll('.mediaItem')).forEach(func
       element.style.webkitTransform = getTransformString()
     }
   })
-  
+
   Object.defineProperty(element.transitions, 'scale3d',{
     get : function () {
       return transitions.scale3d;
@@ -128,7 +117,7 @@ Array.prototype.slice.call(document.querySelectorAll('.mediaItem')).forEach(func
       element.style.webkitTransition = getTransitionString()
     }
   })
-  
+
   Object.defineProperty(element.transitions, 'translate3d',{
     get : function () {
       return transforms.translate3d
@@ -138,5 +127,7 @@ Array.prototype.slice.call(document.querySelectorAll('.mediaItem')).forEach(func
       element.style.webkitTransition = getTransitionString()
     }
   })
-  
-})
+
+}
+
+module.exports = Helper
